6fba515bc92010498d2ee6840cca131e902cf70e
Ridwan <ridwanyinusa15@gmail.com>
2024-08-09 16:57:21 +0100

    chore: add Companies section

---
+++ components/Companies.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+import { companies } from "@/data/data";
+const Companies = () => {
+  return (
+     <main className="padding-x pt-5 sm:pt-10 lg:pt-20 pb-10">
+      <div className="grid grid-cols-6 place-items-center">
+        {companies.map((items, idx: number) => (
+          <div key={idx}>
+            <Image src={items.img} alt={items.img} className=""/>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Companies;
