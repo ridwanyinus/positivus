8837e54a2186b654bef2acff99cd87d8e9aac539
Ridwan <ridwanyinusa15@gmail.com>
2024-08-10 18:47:49 +0100

    chore: add services section for all devices

--- components/Services.tsx
+++ components/Services.tsx
@@ -11,23 +11,23 @@ const Services = () => {
         <p className="heading-sm ">At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. These services include:</p>
       </div>
 
-      <section className="grid grid-cols-2  place-items-center mt-16 gap-y-10 desktop:gap-10">
+      <section className="grid lg:grid-cols-2  place-items-center mt-12 sm:mt-16 gap-y-8 small:gap-y-10 desktop:gap-10">
         {services.map((items, idx: number) => (
           <div
             key={idx}
             style={{ backgroundColor: items.bg }}
-            className={`w-[32rem] desktop:w-[35rem] largesceen:w-[37.5rem] h-[18.125rem] flex justify-between items-center border border-dark rounded-[2.813rem] card-shadow p-8 desktop:p-10 `}>
-            <div className="flex flex-col justify-between gap-y-16 ">
+            className={`w-72 xs:w-80 small:w-[23rem] sm:w-[27rem] xl:w-[32rem] desktop:w-[35rem] largesceen:w-[37.5rem]  sm:h-[14rem] xl:h-[18.125rem] flex justify-between items-center border border-dark rounded-[1.5rem] xs:rounded-[2rem] small:rounded-[2.813rem] card-shadow p-5 xs:p-8 sm:p-8 desktop:p-10 `}>
+            <div className="flex flex-col justify-between gap-y-10 small:gap-y-16 ">
               <div>
-                <h3 className={`${items.headingBg} label font-medium text-3xl `}>{items.heading}</h3>
-                <h3 className={`${items.headingBg} label font-medium text-3xl`}>{items.heading2}</h3>
+                <h3 className={`${items.headingBg} label font-medium text-lg xs:text-xl sm:text-2xl xl:text-3xl `}>{items.heading}</h3>
+                <h3 className={`${items.headingBg} label font-medium text-lg xs:text-xl sm:text-2xl xl:text-3xl`}>{items.heading2}</h3>
               </div>
               <Link href="/" className="flex gap-x-2  items-center">
-                <p className={`text-lg 2xl:text-xl font-normal leading-[140%] ${items.textBg}`}>{items.learn}</p>
+                <p className={`text-sm sm:text-base lg:text-lg 2xl:text-xl font-normal leading-[140%] ${items.textBg}`}>{items.learn}</p>
                 <FaLocationArrow className={`${items.textBg}`} />
               </Link>
             </div>
-            <div>
+            <div className="w-24 small:w-32 sm:w-40 xl:w-auto xl:h-auto">
               <Image src={items.img} alt={items.heading} />
             </div>
           </div>
