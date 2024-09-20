bfe62ee93d227071dde7ebb2c94be725342c7a11
Ridwan <ridwanyinusa15@gmail.com>
2024-09-20 21:36:18 +0100

    chore: add todo

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,11 @@ import Team from "@/components/Team";
 import Testimonials from "@/components/Testimonials";
 import Contacts from "@/components/Contacts";
 import Footer from "@/components/Footer";
+
+// todo: move all lib/sections to their own folder
+// todo: create a utils class for all reusable classes
+// todo: create a utils class for all reusable anim/gsap
+
 export default function Home() {
   return (
     <main className="bg-white">
