e6f12d0e4d5f8e2cf843ddb3b525a28ca55816c5
Ridwan <ridwanyinusa15@gmail.com>
2024-10-01 22:47:26 +0100

    chore: move logo to assets.ts and import

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,11 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { IoClose } from "react-icons/io5";
-import { FaBarsStaggered } from "react-icons/fa6";
-import Link from "next/link";
-import { navItems } from "@/data/data";
-import Image from "next/image";
-import Logo from "@/public/Logo.png";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { IoClose } from 'react-icons/io5';
+import { FaBarsStaggered } from 'react-icons/fa6';
+import Link from 'next/link';
+import { navItems } from '@/data/data';
+import Image from 'next/image';
+import assets from '@/public/assets';
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   const [scroll, setScroll] = useState(false);
@@ -15,66 +15,66 @@ const Navbar = () => {
       setScroll(window.scrollY > 50);
     };
 
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", handleScroll);
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleScroll);
     }
 
     return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("scroll", handleScroll);
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('scroll', handleScroll);
       }
     };
   }, []);
   return (
     <nav className={`pt-6 padding-x w-full relative  overflow-hidden`}>
-      <section className="flex justify-between items-center">
-        <Link href="/" className="relative z-[999]">
-          <Image src={Logo} alt="logo" className="w-36 sm:w-44  largesceen:w-auto" />
+      <section className='flex justify-between items-center'>
+        <Link href='/' className='relative z-[999]'>
+          <Image src={assets.Logo} alt='logo' className='w-36 sm:w-44  largesceen:w-auto' />
         </Link>
 
-        <div className="flex space-x-8 xl:space-x-12 items-center max-lg:hidden">
+        <div className='flex space-x-8 xl:space-x-12 items-center max-lg:hidden'>
           {navItems.map((items, idx: number) => (
             <ul key={`link=${idx}`}>
               <li>
                 <Link
                   href={items.link}
-                  className="text-black  transition-all hover:font-medium hover:border-b border-green text-sm xl:text-[0.938rem] largesceen:text-xl  leading-normal text-right font-normal
-                    ">
+                  className='text-black  transition-all hover:font-medium hover:border-b border-green text-sm xl:text-[0.938rem] largesceen:text-xl  leading-normal text-right font-normal
+                    '>
                   {items.name}
                 </Link>
-                <p className="text-black">{items.img}</p>{" "}
+                <p className='text-black'>{items.img}</p>{' '}
               </li>
             </ul>
           ))}
 
-          <Link href="/" className="border border-dark transition-colors py-2 xl:py-3 px-4 xl:px-6 largesceen:px-8 text-sm xl:text-[0.938rem] largesceen:text-xl text-black rounded-xl hover:bg-green ">
+          <Link href='/' className='border border-dark transition-colors py-2 xl:py-3 px-4 xl:px-6 largesceen:px-8 text-sm xl:text-[0.938rem] largesceen:text-xl text-black rounded-xl hover:bg-green '>
             Request a quote
           </Link>
         </div>
 
         {/* Mobile navbar */}
 
-        <div className="lg:hidden z-[995] relative">
+        <div className='lg:hidden z-[995] relative'>
           {toggleMenu ? (
-            <IoClose size={30} onClick={() => setToggleMenu(false)} className="fixed right-8 top-6 z-10 cursor-pointer text-dark" />
+            <IoClose size={30} onClick={() => setToggleMenu(false)} className='fixed right-8 top-6 z-10 cursor-pointer text-dark' />
           ) : (
-            <FaBarsStaggered size={30} onClick={() => setToggleMenu(true)} className="svg text-dark relative z-10 cursor-pointer" />
+            <FaBarsStaggered size={30} onClick={() => setToggleMenu(true)} className='svg text-dark relative z-10 cursor-pointer' />
           )}
 
           {toggleMenu && (
-            <div className="fixed top-0 right-0 w-[60%] sm:w-1/2 h-screen  overflow-hidden ">
-              <div className="flex flex-col justify-start bg-green items-start w-full h-full pt-28 max-sm:pl-4  sm:px-12 md:px-20 transition-transform transform scale-up-hor-right">
+            <div className='fixed top-0 right-0 w-[60%] sm:w-1/2 h-screen  overflow-hidden '>
+              <div className='flex flex-col justify-start bg-green items-start w-full h-full pt-28 max-sm:pl-4  sm:px-12 md:px-20 transition-transform transform scale-up-hor-right'>
                 {navItems.map((item, idx) => (
-                  <ul key={idx} className="mb-4">
-                    <li className="py-1">
-                      <Link href={item.link} className="text-2xl sm:text-3xl transition-all text-center text-white font-semibold">
+                  <ul key={idx} className='mb-4'>
+                    <li className='py-1'>
+                      <Link href={item.link} className='text-2xl sm:text-3xl transition-all text-center text-white font-semibold'>
                         {item.name}
                       </Link>
                     </li>
                   </ul>
                 ))}
-                <div className="mt-5">
-                  <Link href="/" className="border border-dark transition-colors py-2 px-4 text-xs sm:text-sm  text-black  ">
+                <div className='mt-5'>
+                  <Link href='/' className='border border-dark transition-colors py-2 px-4 text-xs sm:text-sm  text-black  '>
                     Request a quote
                   </Link>
                 </div>
--- public/assets.ts
+++ public/assets.ts
@@ -1,32 +1,34 @@
-import hero from "@/public/hero-Illustration.png";
-import amazon from "@/public/amazonlogo.svg";
-import dribble from "@/public/dribble logo.svg";
-import hubspot from "@/public/hobspotlogo.svg";
-import notion from "@/public/notionlogo.png";
-import netflix from "@/public/netflixlogo.svg";
-import zoom from "@/public/zoomlogo.svg";
-import searchEngine from "@/public/tokyo-magnifier-web-search-with-elements 2.png";
-import payPClick from "@/public/tokyo-selecting-a-value-in-the-browser-window 1.png";
-import socialMedia from "@/public/tokyo-browser-window-with-emoticon-likes-and-stars-around 2.png";
-import mail from "@/public/tokyo-sending-messages-from-one-place-to-another 1.png";
-import content from "@/public/tokyo-many-browser-windows-with-different-information 1.png";
-import analytic from "@/public/tokyo-volumetric-analytics-of-different-types-in-web-browsers 2.png";
-import cta from "@/public/cta.png";
-import plus from "@/public/Plusicon.svg";
-import minus from "@/public/Minusicon.svg";
-import team1 from "@/public/team1.png";
-import team2 from "@/public/team3.png";
-import team3 from "@/public/team2.png";
-import team4 from "@/public/team4.png";
-import team5 from "@/public/team5.png";
-import team6 from "@/public/team6.png";
-import teamLinkedIn from "@/public/linkedinicon.svg";
-import bubble from "@/public/Bubble.png";
-import contactIllustration from "@/public/contactIllustration.png";
-import footerLogo from "@/public/fLogo.png";
+import hero from '@/public/hero-Illustration.png';
+import amazon from '@/public/amazonlogo.svg';
+import dribble from '@/public/dribble logo.svg';
+import hubspot from '@/public/hobspotlogo.svg';
+import notion from '@/public/notionlogo.png';
+import netflix from '@/public/netflixlogo.svg';
+import zoom from '@/public/zoomlogo.svg';
+import searchEngine from '@/public/tokyo-magnifier-web-search-with-elements 2.png';
+import payPClick from '@/public/tokyo-selecting-a-value-in-the-browser-window 1.png';
+import socialMedia from '@/public/tokyo-browser-window-with-emoticon-likes-and-stars-around 2.png';
+import mail from '@/public/tokyo-sending-messages-from-one-place-to-another 1.png';
+import content from '@/public/tokyo-many-browser-windows-with-different-information 1.png';
+import analytic from '@/public/tokyo-volumetric-analytics-of-different-types-in-web-browsers 2.png';
+import cta from '@/public/cta.png';
+import plus from '@/public/Plusicon.svg';
+import minus from '@/public/Minusicon.svg';
+import team1 from '@/public/team1.png';
+import team2 from '@/public/team3.png';
+import team3 from '@/public/team2.png';
+import team4 from '@/public/team4.png';
+import team5 from '@/public/team5.png';
+import team6 from '@/public/team6.png';
+import teamLinkedIn from '@/public/linkedinicon.svg';
+import bubble from '@/public/Bubble.png';
+import contactIllustration from '@/public/contactIllustration.png';
+import footerLogo from '@/public/fLogo.png';
+import Logo from '@/public/Logo.png';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+  Logo,
   hero,
   amazon,
   dribble,
