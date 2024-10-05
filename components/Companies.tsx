b73002908303a8e7bf20729b2dcc3754d9e7c981
Ridwan <ridwanyinusa15@gmail.com>
2024-10-05 23:20:23 +0100

    chore: move data to lib

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { companies } from "@/data/data";
-import InfiniteMovingCards from "./ui/InfinityMovingCard";
+import { companies } from '@/lib/content/companies';
+import InfiniteMovingCards from './ui/InfinityMovingCard';
+
 const Companies = () => {
   return (
-    <main className="padding-x py-3 sm:py-8 lg:py-10 xl:py-16 largesceen:py-20">
-      <div className="flex place-items-center">
+    <main className='padding-x py-3 sm:py-8 lg:py-10 xl:py-16 largesceen:py-20'>
+      <div className='flex place-items-center'>
         <InfiniteMovingCards items={companies} />
       </div>
     </main>
---
+++ lib/content/companies.ts
@@ -0,0 +1,4 @@
+import assets from '@/public/assets';
+import { companiesType } from '../types/sections';
+
+export const companies: companiesType[] = [{ img: assets.amazon }, { img: assets.dribble }, { img: assets.hubspot }, { img: assets.notion }, { img: assets.netflix }, { img: assets.zoom }];
